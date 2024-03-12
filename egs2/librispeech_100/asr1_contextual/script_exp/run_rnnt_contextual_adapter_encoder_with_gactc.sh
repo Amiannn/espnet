8f8d0334c3b4af9c301aa2509b6ad5359acc9776
Amiannn <wang.amiannn@gmail.com>
2024-03-13 02:23:15 +0800

    Change attention funct into espnet build in funct

--- egs2/librispeech_100/asr1_contextual/script_exp/run_rnnt_contextual_adapter_encoder_with_gactc.sh
+++ egs2/librispeech_100/asr1_contextual/script_exp/run_rnnt_contextual_adapter_encoder_with_gactc.sh
@@ -9,13 +9,13 @@ train_set="train_clean_100"
 valid_set="dev"
 test_sets="test_clean"
 
-asr_config=conf/exp/contextual_adapter/train_rnnt_contextual_adapter_encoder.yaml
+asr_config=conf/exp/contextual_adapter/train_rnnt_contextual_adapter_encoder_with_gactc.yaml
 inference_config=conf/exp/decode_asr_contextual_rnnt_transducer_greedy.yaml
-asr_tag=finetune_freeze_ct_enc_cb
+asr_tag=finetune_freeze_ct_enc_cb_gactc
 
 pretrained_model=exp/asr_pretrain_rnnt/valid.loss.10epoch.pth
 
-CUDA_VISIBLE_DEVICES=0 ./asr.sh \
+CUDA_VISIBLE_DEVICES=1 ./asr.sh \
     --lang en \
     --ngpu 1 \
     --nj 16 \
@@ -39,7 +39,7 @@ CUDA_VISIBLE_DEVICES=0 ./asr.sh \
     --pretrained_model $pretrained_model \
     --ignore_init_mismatch true \
     --inference_asr_model valid.loss.ave_10best.pth \
+    --asr_args "--use_wandb true --wandb_project Contextualize_RNNT" \
     --asr_tag ${asr_tag} \
     "$@"
 
-    # --asr_args "--use_wandb true --wandb_project Contextualize_RNNT" \
