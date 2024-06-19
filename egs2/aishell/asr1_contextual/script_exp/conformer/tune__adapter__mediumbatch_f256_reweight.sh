618aec560e3c2d8dd9cd30090a72345a89222cbf
Amiannn <wang.amiannn@gmail.com>
2024-06-20 01:35:49 +0800

    feat: add objective for contextualizer

--- egs2/aishell/asr1_contextual/script_exp/conformer/tune__adapter__mediumbatch_f256_reweight.sh
+++ egs2/aishell/asr1_contextual/script_exp/conformer/tune__adapter__mediumbatch_f256_reweight.sh
@@ -9,9 +9,9 @@ train_set=train
 valid_set=dev
 test_sets="test"
 
-asr_config=conf/contextual/conformer/adapter__mediumbatch.yaml
-inference_config=conf/contextual/conformer/decode_asr_conformer_adapter_bs10.yaml
-asr_tag=conformer/adapter__mediumbatch
+asr_config=conf/contextual/conformer/adapter__mediumbatch_f256_reweight.yaml
+inference_config=conf/contextual/conformer/decode_asr_conformer_adapter_bs5.yaml
+asr_tag=conformer/adapter__mediumbatch_f256_reweight
 
 lm_config=conf/train_lm_transformer.yaml
 use_lm=false
